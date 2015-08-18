3847af6e67c4fd9da43d0dabfdc8d8207014574e
andy-dev <andres.macedo@gmail.com>
2015-08-18 15:15:28 -0700

    add background image

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var GameState = {
   preload: function(){
+    this.load.image('background', 'assets/images/background.png')
 
   },
   create: function(){
-
+    this.background = this.game.add.sprite(0,0,'background')
   },
   update: function(){
 
