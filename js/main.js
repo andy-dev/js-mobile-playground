580e6a0a11108651bcd9aac6b99950b6d2d48d77
andy-dev <andres.macedo@gmail.com>
2015-08-18 16:27:37 -0700

    add arrow png and started basic input recognition for user

--- js/main.js
+++ js/main.js
@@ -1,32 +1,68 @@
 var GameState = {
-  preload: function(){
-    this.load.image('background', 'assets/images/background.png');
-    this.load.image('chicken', 'assets/images/chicken.png');
-    this.load.image('horse', 'assets/images/horse.png');
-    this.load.image('pig', 'assets/images/pig.png');
-    this.load.image('sheep', 'assets/images/sheep3.png');
+  //load the game assets before the game starts
+  preload: function() {
+    this.game.load.image('background', 'assets/images/background.png');
+    this.game.load.image('arrow', 'assets/images/arrow.png');
+    this.game.load.image('chicken', 'assets/images/chicken.png');
+    this.game.load.image('horse', 'assets/images/horse.png');
+    this.game.load.image('pig', 'assets/images/pig.png');
+    this.game.load.image('sheep', 'assets/images/sheep3.png');
 
   },
-  create: function(){
+  //executed after everything is loaded
+  create: function() {
 
+    //scaling options
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+    //have the game centered horizontally
+    this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
-    this.background = this.game.add.sprite(0,0,'background');
-    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken')
-    this.chicken.anchor.setTo(0.5, 0.5); //change location of anchor point to center
-    this.chicken.scale.setTo(0.5);
 
-    this.horse = this.game.add.sprite(500,300, 'horse');
-    this.horse.anchor.setTo(0.5);
-    this.horse.scale.setTo(1,1) //flip on x stay the same on y
+    //create a sprite for the background
+    this.background = this.game.add.sprite(0, 0, 'background')
 
-    this.sheep = this.game.add.sprite(100,250, 'sheep');
-    this.sheep.scale.setTo(0.5);
-    this.sheep.angle = -45;
+    this.pig = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'pig');
+    this.pig.anchor.setTo(0.5);
+
+    //enable user input on sprite
+    this.pig.inputEnabled = true;
+    this.pig.input.pixelPerfectClick = true;
+    this.pig.events.onInputDown.add(this.animateAnimal, this);
+
+    //left arrow
+    this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
+    this.leftArrow.anchor.setTo(0.5);
+    this.leftArrow.scale.x = -1;
+    this.leftArrow.customParams = {direction: -1};
+
+    //left arrow allow user input
+    this.leftArrow.inputEnabled = true;
+    this.leftArrow.input.pixelPerfectClick = true;
+    this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+    //right arrow
+    this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
+    this.rightArrow.anchor.setTo(0.5);
+    this.rightArrow.customParams = {direction: 1};
+
+    //right arrow user input
+    this.rightArrow.inputEnabled = true;
+    this.rightArrow.input.pixelPerfectClick = true;
+    this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
+
   },
-  update: function(){
-    this.sheep.angle += 0.5;
+  //this is executed multiple times per second
+  update: function() {
+  },
+  switchAnimal: function(sprite, event) {
+    console.log('move animal');
+  },
+  animateAnimal: function(sprite, event) {
+    console.log('animate animal');
   }
+
+
 };
 
 var game = new Phaser.Game(640, 360, Phaser.AUTO);
