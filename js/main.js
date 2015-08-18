fd87a409b90f29a6e69a4bcb6fe4be41a3de5770
andy-dev <andres.macedo@gmail.com>
2015-08-18 15:23:06 -0700

    add images of animals, played with anchor point

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 var GameState = {
   preload: function(){
-    this.load.image('background', 'assets/images/background.png')
+    this.load.image('background', 'assets/images/background.png');
+    this.load.image('chicken', 'assets/images/chicken.png');
+    this.load.image('horse', 'assets/images/horse.png');
+    this.load.image('pig', 'assets/images/pig.png');
+    this.load.image('sheep3', 'assets/images/sheep3.png');
 
   },
   create: function(){
-    this.background = this.game.add.sprite(0,0,'background')
+    this.background = this.game.add.sprite(0,0,'background');
+    this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken')
+    this.chicken.anchor.setTo(0.5, 0.5); //change location of anchor point to center
   },
   update: function(){
 
