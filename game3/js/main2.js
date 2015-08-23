a61cd73db73513d89a81c9d3c38edf7569853452
andy-dev <andres.macedo@gmail.com>
2015-08-22 20:15:26 -0700

    modify assets and js for initial game background

--- game3/js/main2.js
+++ game3/js/main2.js
@@ -1,11 +1,29 @@
 var GameState = {
   init: function(){
-
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
   },
   preload: function(){
 
+    this.load.image('backyard', 'assets/images/backyard.png');
+    this.load.image('apple', 'assets/images/apple.png');
+    this.load.image('candy', 'assets/images/candy.png');
+    this.load.image('rotate', 'assets/images/rotate.png');
+    this.load.image('toy', 'assets/images/rubber_duck.png');
+    this.load.image('arrow', 'assets/images/arrow.png');
+    this.load.spritesheet('pet', 'assets/images/pet.png', 97, 83, 5, 1, 1);
+
   },
   create: function(){
+    this.background = this.game.add.sprite(0,0, 'backyard');
+    this.pet = this.game.add.sprite(100, 400, 'pet');
+    this.pet.anchor.setTo(0.5);
+    this.pet.customParams = {health: 100, fun: 100};
+    this.apple = this.game.add.sprite(72,570, 'apple');
+    this.candy = this.game.add.sprite(144,570, 'candy');
+    this.toy = this.game.add.sprite(216,570, 'toy');
+    this.rotate = this.game.add.sprite(288,570, 'rotate');
 
   }
 };
