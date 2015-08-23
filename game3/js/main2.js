53b82553b29cec93648e6d006e0fde0224d84b47
andy-dev <andres.macedo@gmail.com>
2015-08-22 19:58:53 -0700

    modified basic setup add phaser.js and assets for the game

---
+++ game3/js/main2.js
@@ -0,0 +1,17 @@
+var GameState = {
+  init: function(){
+
+  },
+  preload: function(){
+
+  },
+  create: function(){
+
+  }
+};
+
+
+
+var game = new Phaser.Game(360, 640, Phaser.AUTO);
+game.state.add('GameState', GameState);
+game.state.start('GameState');
